0a6e3264c35da5238bdd13ca2d614f4ed1a54c59
Nick Panteleeff <drnix@chess-online.com>
2018-05-07 19:14:12 +0300

    Fix links

--- modules/evaluation/src/main/PlayerAggregateAssessment.scala
+++ modules/evaluation/src/main/PlayerAggregateAssessment.scala
@@ -121,7 +121,7 @@ case class PlayerAggregateAssessment(
 
   def reportText(reason: String, maxGames: Int = 10): String = {
     val gameLinks: String = (playerAssessments.sortBy(-_.assessment.id).take(maxGames).map { a =>
-      a.assessment.emoticon + " lichess.org/" + a.gameId + "/" + a.color.name
+      a.assessment.emoticon + " live.chess-online.com/" + a.gameId + "/" + a.color.name
     }).mkString("\n")
 
     s"""[AUTOREPORT] $reason
