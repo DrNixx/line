609c7cf3689e0b679bfd9f4abd440e0a2157ef04
Nick Panteleeff <drnix@chess-online.com>
2018-05-07 14:13:16 +0300

    Load chess-online game pgn from game link

--- modules/study/src/main/PgnFetch.scala
+++ modules/study/src/main/PgnFetch.scala
@@ -13,13 +13,27 @@ private final class PgnFetch {
   // http://www.chessgames.com/pgn/boleslavsky_ufimtsev_1944.pgn?gid=1427487
   private val ChessbaseRegex = """.*chessgames\.com/.*[\?&]gid=(\d+).*""".r
 
+  // https://www.chess-online.com/ru-ru/7697348
+  // https://backend.chess-online.com/ru-ru/arbiter/game/7697348
+  // https://backend.chess-online.com/ru-ru/arbiter/game?id=7697348
+  private val Chessonline1Regex = """https://www\.chess-online\.com/[a-z]{2,3}(?:-[a-z]{2,3})?/(\d+)""".r
+  private val Chessonline2Regex = """https://backend\.chess-online\.com/[a-z]{2,3}(?:-[a-z]{2,3})?/arbiter/game(?:/|\?id=)(\d+)""".r
+
   def fromUrl(url: String): Fu[Option[Pgn]] = url match {
+    case Chessonline1Regex(id) => parseIntOption(id) ?? downloadChessonline
+    case Chessonline2Regex(id) => parseIntOption(id) ?? downloadChessonline
     case ChessbaseRegex(id) => parseIntOption(id) ?? downloadChessbase
     case _ => fuccess(none)
   }
 
   private def downloadChessbase(id: Int): Fu[Option[Pgn]] = {
     WS.url(s"""http://www.chessgames.com/pgn/any.pgn?gid=$id""").get().map { res =>
+      res.header("Content-Type").contains(pgnContentType) option res.body
+    }
+  }
+
+  private def downloadChessonline(id: Int): Fu[Option[Pgn]] = {
+    WS.url(s"""https://www.chess-online.com/chess/pgn/$id""").get().map { res =>
       res.header("Content-Type").contains(pgnContentType) option res.body
     }
   }
