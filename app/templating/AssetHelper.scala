9bb1192ace432742b004e0b4ade24abe2ee7b89d
DrNixx <drnixx@gmail.com>
2020-09-28 14:27:33 +0300

    Fix

--- app/templating/AssetHelper.scala
+++ app/templating/AssetHelper.scala
@@ -96,7 +96,7 @@ trait AssetHelper { self: I18nHelper with SecurityHelper =>
     }
     ContentSecurityPolicy(
       defaultSrc = List("'self'", assets),
-      connectSrc = "'self'" :: assets :: sockets ::: env.explorerEndpoint :: env.tablebaseEndpoint :: "https://www.chess-online.com" :: Nil,
+      connectSrc = "'self'" :: assets :: sockets ::: env.explorerEndpoint :: env.tablebaseEndpoint :: "https://www.chess-online.com" :: "https://www.google-analytics.com" :: Nil,
       styleSrc = List("'self'", "'unsafe-inline'", assets),
       fontSrc = List("'self'", assetDomain.value, "https://fonts.gstatic.com"),
       frameSrc = List("'self'", assets, "https://www.youtube.com", "https://player.twitch.tv"),
